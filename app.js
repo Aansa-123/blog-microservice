66d54e744f12c5e8e793f8ce72fdfed6de16c186
Aansa-123 <aansarana786@gmail.com>
2026-08-10 14:22:33 +0500

    add metrics

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 
 // Prometheus metrics collection
-client.collectDefaultMetrics();
+const register = new client.Registry();
+client.collectDefaultMetrics({ 
+    register 
+});
 //cors
 app.use(cors({
     origin: "http://localhost:5173",
@@ -27,10 +30,42 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // Prometheus metrics endpoint
-app.get("/metrics", async (req, res) => {
-  res.set("Content-Type", client.register.contentType);
-  res.end(await client.register.metrics());
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "HTTP request duration in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.1, 0.5, 1, 2, 5],
+  registers: [register]
 });
+
+app.use((req, res, next) => {
+  const start = process.hrtime();
+
+  res.on("finish", () => {
+    const duration = process.hrtime(start);
+    const durationInSeconds =
+      duration[0] + duration[1] / 1e9;
+
+    httpRequestDuration.observe(
+      {
+        method: req.method,
+        route: req.route?.path || req.path,
+        status_code: res.statusCode,
+      },
+      durationInSeconds
+    );
+  });
+
+  next();
+});
+
+
+// Prometheus metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
 
 
 // routes
